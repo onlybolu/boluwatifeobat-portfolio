bfa78d74bb107321e35fdf8b1e935901a73e198b
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-04-15 13:51:03 -0700

    false

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -11,7 +11,7 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in the .env file, refer to .env.example
 const protectedRoutes = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": false,
 };
 
 const style = {
